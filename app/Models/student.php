8212b61754843da1bcda09ea5ef7fb07cdd90c44
rikkyvish94 <ramakant.vishwakarma@impactguru.com>
2023-01-24 14:25:24 +0530

    dd

--- app/Models/student.php
+++ app/Models/student.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class student extends Model
+
+class student extends Model implements JWTSubject
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
 }
