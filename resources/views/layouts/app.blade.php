edb4f8f6d71945a747a8b68e80eba89d0fa53b75
Ramakant Vishwakarma <rkvis@Ramakant.localdomain>
2023-01-30 00:34:04 +0530

    dd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,39 +8,39 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>Library| Dashboard</title>
-    <link rel="icon" href="{{asset('public/asset/img/logo.png') }}" type="image/png">
+    <link rel="icon" href="{{asset('/asset/img/logo.png') }}" type="image/png">
 
-    <link rel="stylesheet" href="{{asset('public/asset/css/bootstrap1.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/css/bootstrap1.min.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/themefy_icon/themify-icons.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/themefy_icon/themify-icons.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/swiper_slider/css/swiper.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/swiper_slider/css/swiper.min.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/select2/css/select2.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/select2/css/select2.min.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/niceselect/css/nice-select.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/niceselect/css/nice-select.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/owl_carousel/css/owl.carousel.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/owl_carousel/css/owl.carousel.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/gijgo/gijgo.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/gijgo/gijgo.min.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/font_awesome/css/all.min.css') }}" />
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/tagsinput/tagsinput.cs') }}s" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/font_awesome/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/tagsinput/tagsinput.cs') }}s" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/datatable/css/jquery.dataTables.min.css') }}" />
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/datatable/css/responsive.dataTables.min.css') }}" />
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/datatable/css/buttons.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/datatable/css/jquery.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/datatable/css/responsive.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/datatable/css/buttons.dataTables.min.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/text_editor/summernote-bs4.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/text_editor/summernote-bs4.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/morris/morris.css') }}">
+    <link rel="stylesheet" href="{{asset('/asset/vendors/morris/morris.css') }}">
 
-    <link rel="stylesheet" href="{{asset('public/asset/vendors/material_icon/material-icons.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/vendors/material_icon/material-icons.css') }}" />
 
-    <link rel="stylesheet" href="{{asset('public/asset/css/metisMenu.css') }}">
+    <link rel="stylesheet" href="{{asset('/asset/css/metisMenu.css') }}">
 
-    <link rel="stylesheet" href="{{asset('public/asset/css/style1.css') }}" />
-    <link rel="stylesheet" href="{{asset('public/asset/css/colors/default.css') }}" id="colorSkinCSS">
+    <link rel="stylesheet" href="{{asset('/asset/css/style1.css') }}" />
+    <link rel="stylesheet" href="{{asset('/asset/css/colors/default.css') }}" id="colorSkinCSS">
 </head>
 
 <body class="crm_body_bg">
@@ -53,49 +53,49 @@
 
 
 
-    <script src="{{asset('public/asset/js/jquery1-3.4.1.min.js') }}"></script>
+    <script src="{{asset('/asset/js/jquery1-3.4.1.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/js/popper1.min.js') }}"></script>
+    <script src="{{asset('/asset/js/popper1.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/js/bootstrap1.min.js') }}"></script>
+    <script src="{{asset('/asset/js/bootstrap1.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/js/metisMenu.js') }}"></script>
+    <script src="{{asset('/asset/js/metisMenu.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/count_up/jquery.waypoints.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/count_up/jquery.waypoints.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/chartlist/Chart.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/chartlist/Chart.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/count_up/jquery.counterup.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/count_up/jquery.counterup.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/swiper_slider/js/swiper.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/swiper_slider/js/swiper.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/niceselect/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/niceselect/js/jquery.nice-select.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/owl_carousel/js/owl.carousel.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/owl_carousel/js/owl.carousel.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/gijgo/gijgo.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/gijgo/gijgo.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/datatable/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/buttons.flash.min.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/jszip.min.js') }}') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/pdfmake.min.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/vfs_fonts.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/buttons.html5.min.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/datatable/js/buttons.print.min.js') }}"></script>
-    <script src="{{asset('public/asset/js/chart.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/buttons.flash.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/jszip.min.js') }}') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/pdfmake.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/vfs_fonts.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{asset('/asset/vendors/datatable/js/buttons.print.min.js') }}"></script>
+    <script src="{{asset('/asset/js/chart.min.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/progressbar/jquery.barfiller.js') }}"></script>
+    <script src="{{asset('/asset/vendors/progressbar/jquery.barfiller.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/tagsinput/tagsinput.js') }}"></script>
+    <script src="{{asset('/asset/vendors/tagsinput/tagsinput.js') }}"></script>
 
-    <script src="{{asset('public/asset/vendors/text_editor/summernote-bs4.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/apex_chart/apexcharts.js') }}"></script>
+    <script src="{{asset('/asset/vendors/text_editor/summernote-bs4.js') }}"></script>
+    <script src="{{asset('/asset/vendors/apex_chart/apexcharts.js') }}"></script>
 
-    <script src="{{asset('public/asset/js/custom.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/apex_chart/bar_active_1.js') }}"></script>
-    <script src="{{asset('public/asset/vendors/apex_chart/apex_chart_list.js') }}"></script>
+    <script src="{{asset('/asset/js/custom.js') }}"></script>
+    <script src="{{asset('/asset/vendors/apex_chart/bar_active_1.js') }}"></script>
+    <script src="{{asset('/asset/vendors/apex_chart/apex_chart_list.js') }}"></script>
 </body>
 
 </html>
